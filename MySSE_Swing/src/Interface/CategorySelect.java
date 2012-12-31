913b24c05d16c5d7f9b873b64556d7f1a1c247e5
林宜葳 <1014582610@qq.com>
2012-12-31 19:27:58 +0800

    类

--- MySSE_Swing/src/Interface/CategorySelect.java
+++ MySSE_Swing/src/Interface/CategorySelect.java
@@ -3,6 +3,12 @@
  */
 package Interface;
 
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -17,11 +23,51 @@ public class CategorySelect extends JPanel {
 	private static final long serialVersionUID = -833635473722496397L;
 
 	private TitleTablePanel delegateTiTleTablePanel;
-	/**
+    private JButton notice=new JButton();
+    private JButton news=new JButton();
+    private JButton activity=new JButton();
+    private ImageIcon notice_img=new ImageIcon("C:\\Users\\lenovo\\Documents\\GitHub\\MySSE\\MySSE_Swing\\src\\Interface\\116.gif");
+    
+	/**	
 	 * 
 	 */
-	public CategorySelect() {
+	public CategorySelect(TitleTablePanel _delegateTitleTablePanel) {
+            this.delegateTiTleTablePanel=_delegateTitleTablePanel;
+            notice.addActionListener(new ButtonAction());
+            news.addActionListener(new ButtonAction());
+            activity.addActionListener(new ButtonAction());
+            
+            this.setLayout(null);
+            notice.setBounds(new Rectangle(450,25,120,50));
+            news.setBounds(new Rectangle(620,25,120,50));
+            activity.setBounds(new Rectangle(790,25,120,50));
+            notice.setIcon(notice_img);
+            
+            this.add(notice);
+            this.add(news);
+            this.add(activity);
 		// TODO Auto-generated constructor stub
 	}
-
+        class ButtonAction implements ActionListener
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                Object name=e.getSource();
+                if(name == notice)
+                {
+                	int i=0;
+                    delegateTiTleTablePanel.select(i);
+                }
+                else if(name==news)
+                {
+                	int i=1;
+                    delegateTiTleTablePanel.select(i);
+                }
+                else if(name==activity)
+                {
+                	int i=2;
+                    delegateTiTleTablePanel.select(i);
+                }
+            }
+        }
 }
--- MySSE_Swing/src/Interface/MainFrame.java
+++ MySSE_Swing/src/Interface/MainFrame.java
@@ -6,6 +6,8 @@ package Interface;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 /**
  * @author Mark
@@ -30,6 +32,24 @@ public class MainFrame extends JFrame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		JFrame jf=new JFrame();
+		jf.setBounds(0, 0, 1360, 720);
+		jf.setVisible(true);
+		JPanel p=new JPanel();
+		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		ContentPanel content=new ContentPanel();
+		TitleTablePanel title=new TitleTablePanel(content);
+		CategorySelect category=new CategorySelect(title);
+		category.setBounds(0, 0, 1360, 100);
+		category.setBorder(new TitledBorder("1"));
+		title.setBounds(0, 100, 500, 620);
+		title.setBorder(new TitledBorder("2"));
+		content.setBounds(500, 100, 860, 620);
+		content.setBorder(new TitledBorder("3"));
+		jf.add(category);
+		jf.add(title);
+		jf.add(content);
+		jf.add(p);
 		// TODO Auto-generated method stub
 
 	}
